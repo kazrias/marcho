ff273e1ec05072f271821fa42a4c610dc173fbd4
kazrias <ka6rias@gmail.com>
2022-09-30 06:08:59 +0400

    top-slider section created

---
+++ app/js/main.js
@@ -0,0 +1,12 @@
+$(function () {
+  $('.top-slider__inner').slick({
+    dots: true,
+    arrows: false,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    autoplay: true,
+    autoplaySpeed: 2000,
+  })
+});
